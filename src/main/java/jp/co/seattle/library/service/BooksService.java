4e24530b5c79c486e713597b306df76f2e0cffc1
tanakamoe <moe.tanaka@seattleconsulting.co.jp>
2021-04-19 16:11:26 +0900

    一旦避難

--- src/main/java/jp/co/seattle/library/service/BooksService.java
+++ src/main/java/jp/co/seattle/library/service/BooksService.java
@@ -33,7 +33,7 @@ public class BooksService {
 
         // TODO 取得したい情報を取得するようにSQLを修正
         List<BookInfo> getedBookList = jdbcTemplate.query(
-                "SELECT TITLE,AUTHOR,PUBLISHER,PUBLISH_DATE FROM books ORDER BY ASC",
+                "select id,title,author,publisher,publish_date,thumbnail_url from books order by title asc",
                 new BookInfoRowMapper());
 
         return getedBookList;
