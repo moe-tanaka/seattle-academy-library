ab36bb89149576b033fde2b1c81f02455c7dc1cc
tanakamoe <moe.tanaka@seattleconsulting.co.jp>
2021-04-20 16:46:47 +0900

    タスク4完了

--- src/main/java/jp/co/seattle/library/service/BooksService.java
+++ src/main/java/jp/co/seattle/library/service/BooksService.java
@@ -74,4 +74,9 @@ public class BooksService {
 
         jdbcTemplate.update(sql);
     }
+
+    public void deleteBook(int bookId) {
+        String sql = "delete from books where id  = '" + bookId + "'";
+        jdbcTemplate.update(sql);
+    }
 }
